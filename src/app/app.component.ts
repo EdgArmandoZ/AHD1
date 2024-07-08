b9acb3efabaa28515ed0592afb186b7e4d76809f
EdgArmandoZ <zapetaedgar5@gmail.com>
2024-07-08 13:11:43 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 import { Formulario2Component } from './formulario2/formulario2.component';
 
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Formulario2Component],
+  imports: [Formulario2Component, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
